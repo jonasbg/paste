0e536e64183e16f14c91539d3e4432c3c67c833f
Jonas August Staff <jonasbg@users.noreply.github.com>
2025-02-08 19:40:24 +0100

    request log everything

--- api/handlers/metrics.go
+++ api/handlers/metrics.go
@@ -2,31 +2,33 @@ package handlers
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jonasbg/paste/m/v2/db"
+	"github.com/jonasbg/paste/m/v2/types"
 	"github.com/jonasbg/paste/m/v2/utils"
 )
 
 func HandleSecurityMetrics(db *db.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-			var start, end time.Time
+		var start, end time.Time
 
-			if rangeStr := c.Query("range"); rangeStr != "" {
-					start, end = utils.ParseTimeRange(rangeStr)
-			} else {
-					start, _ = time.Parse(time.RFC3339, c.Query("start"))
-					end, _ = time.Parse(time.RFC3339, c.Query("end"))
-			}
+		if rangeStr := c.Query("range"); rangeStr != "" {
+			start, end = utils.ParseTimeRange(rangeStr)
+		} else {
+			start, _ = time.Parse(time.RFC3339, c.Query("start"))
+			end, _ = time.Parse(time.RFC3339, c.Query("end"))
+		}
 
-			metrics, err := db.GetSecurityMetrics(start, end)
-			if err != nil {
-					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-					return
-			}
+		metrics, err := db.GetSecurityMetrics(start, end)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 
-			c.JSON(http.StatusOK, metrics)
+		c.JSON(http.StatusOK, metrics)
 	}
 }
 
@@ -51,21 +53,40 @@ func HandleActivity(db *db.DB) gin.HandlerFunc {
 	}
 }
 
-func HandleStorage(db *db.DB) gin.HandlerFunc {
+func HandleStorage(uploadDir string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var start, end time.Time
-
-		if rangeStr := c.Query("range"); rangeStr != "" {
-			start, end = utils.ParseTimeRange(rangeStr)
-		} else {
-			start, _ = time.Parse(time.RFC3339, c.Query("start"))
-			end, _ = time.Parse(time.RFC3339, c.Query("end"))
+		summary := types.StorageSummary{
+			FileSizeDistribution: make(map[string]int),
 		}
 
-		summary, err := db.GetStorageSummary(start, end)
+		entries, err := os.ReadDir(uploadDir)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
+		}
+
+		for _, entry := range entries {
+			info, err := entry.Info()
+			if err != nil {
+				continue
+			}
+
+			// Update file count and total size
+			summary.TotalFiles++
+			summary.TotalSizeBytes += float64(info.Size())
+
+			// Categorize file size
+			size := info.Size()
+			switch {
+			case size < 1024*1024: // < 1MB
+				summary.FileSizeDistribution["0-1MB"]++
+			case size < 10*1024*1024: // < 10MB
+				summary.FileSizeDistribution["1-10MB"]++
+			case size < 100*1024*1024: // < 100MB
+				summary.FileSizeDistribution["10-100MB"]++
+			default:
+				summary.FileSizeDistribution["100MB+"]++
+			}
 		}
 
 		c.JSON(http.StatusOK, summary)
